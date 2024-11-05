cb059a25b863cbc5d94e26fd55e60cbe95efcdd2
Eric-Formiga <eric.13formiga@gmail.com>
2024-11-04 22:27:44 -0300

    feat:add Auth state

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { useState } from "react"
+
 
 const index = () => {
+  const [name] = useState('')
   return (
     <div>
-      ...
+      {name}
     </div>
   )
 }
