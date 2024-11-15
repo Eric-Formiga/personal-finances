9c9eeeb55874f24a17bf0e73594f88f971dd810e
Eric-Formiga <eric.13formiga@gmail.com>
2024-11-14 22:10:06 -0300

    feat: add new transaction page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import { Auth } from "../pages/Auth"
 import { Layout } from "../components/Layout"
 import { Home } from "../pages/Home"
+import { NewTransaction } from "../pages/Transaction/New"
 
 export const MainRoutes = () => {
     return (
@@ -17,10 +18,17 @@ export const MainRoutes = () => {
             />
 
             <Route element={<Layout />}>
-                <Route 
-                index
-                element={<Home/>}
+                <Route
+                    index
+                    element={<Home />}
                 />
+
+                <Route path="/transacoes">
+                    <Route
+                        path="nova"
+                        element={<NewTransaction />}
+                    />
+                </Route>
             </Route>
         </Routes>
     )
