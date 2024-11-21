395507e392823c113517918946813e980cbd4716
Eric-Formiga <eric.13formiga@gmail.com>
2024-11-21 20:59:49 -0300

    feat: add Account page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,7 @@ import { Home } from "../pages/Home"
 import { NewTransaction } from "../pages/Transaction/New"
 import { EditTransaction } from "../pages/Transaction/Edit"
 import { Transactions } from "../pages/Transaction/Transactions"
+import { Account } from "../pages/Account"
 
 export const MainRoutes = () => {
     return (
@@ -23,6 +24,10 @@ export const MainRoutes = () => {
                 <Route
                     index
                     element={<Home />}
+                />
+                <Route
+                    path="/account"
+                    element={<Account />}
                 />
                 <Route path="/transacoes">
                     <Route
